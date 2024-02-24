4760d31b2588251f11e6678ae2518b16a57b2770
jungha.so <pansso@naver.com>
2024-02-24 09:33:49 +0900

    add OpenWeatherApi EndPoint

--- data/src/main/java/com/example/data/api/EndPoint.kt
+++ data/src/main/java/com/example/data/api/EndPoint.kt
@@ -2,4 +2,5 @@ package com.example.data.api
 
 object EndPoint{
     const val githubEndpoint = "https://api.github.com"
+    const val openWeatherEndPoint = "https://api.openweathermap.org"
 }
---
+++ data/src/main/java/com/example/data/api/OpenWeatherAPI.kt
@@ -0,0 +1,21 @@
+package com.example.data.api
+
+import com.example.data.model.openweather.WeatherResponseDTO
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface OpenWeatherAPI {
+    @GET("data/2.5/weather")
+    suspend fun fetchWeatherByLatLng(
+        @Query("lat") lat: Double,
+        @Query("lng") lng: Double,
+        @Query("appid") appid: String
+    ): WeatherResponseDTO
+
+    @GET("data/2.5/weather")
+    suspend fun fetchWeatherByCountry(
+        @Query("q") cityName: String,
+        @Query("appid") appid: String
+    ): WeatherResponseDTO
+}
