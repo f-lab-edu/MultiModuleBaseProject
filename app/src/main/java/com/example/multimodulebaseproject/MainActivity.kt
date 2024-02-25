06512839678f6af039638eff6ca2290ed5c261f7
jungha.so <pansso@naver.com>
2024-02-25 10:16:04 +0900

    ui layer inject UseCase

--- app/src/main/java/com/example/multimodulebaseproject/MainActivity.kt
+++ app/src/main/java/com/example/multimodulebaseproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multimodulebaseproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.multimodulebaseproject.ui.theme.MultiModuleBaseProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val viewModel: MainViewModel = hiltViewModel()
                     Greeting("Android")
                 }
             }
