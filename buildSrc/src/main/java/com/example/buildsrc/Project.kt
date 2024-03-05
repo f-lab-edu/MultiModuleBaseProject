6da8581156b01f3bb0fe9636250f182431d862df
jungha.so <pansso@naver.com>
2024-03-05 17:45:54 +0900

    #3 - presentation module 추가

--- buildSrc/src/main/java/com/example/buildsrc/Project.kt
+++ buildSrc/src/main/java/com/example/buildsrc/Project.kt
@@ -4,4 +4,5 @@ object Project {
     const val app = ":app"
     const val domain = ":domain"
     const val data = ":data"
+    const val presentation = ":presentation"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,4 @@ rootProject.name = "MultiModuleBaseProject"
 include(":app")
 include(":domain")
 include(":data")
+include(":presentation")
