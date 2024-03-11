fbf9b28b0f89afa40939e18ae9d906ef390e93ce
jungha.so <pansso@naver.com>
2024-03-12 03:08:59 +0900

    #7 mapper class 위치 data -> domain 으로 변경

--- domain/src/main/java/com/example/domain/usecase/openweather/FetchWeatherByCityUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/openweather/FetchWeatherByCityUseCase.kt
@@ -2,6 +2,7 @@ package com.example.domain.usecase.openweather
 
 import com.example.domain.model.openweather.WeatherData
 import com.example.data.repository.OpenWeatherRepository
+import com.example.domain.model.openweather.toDomain
 import com.example.domain.usecase.utils.UseCase
 import javax.inject.Inject
 
@@ -11,6 +12,6 @@ class FetchWeatherByCityUseCase @Inject constructor(
     data class Param(val city: String)
 
     override suspend fun execute(param : Param): WeatherData {
-       return repository.fetchWeatherByCity(param.city)
+       return repository.fetchWeatherByCity(param.city).toDomain()
     }
 }
--- presentation/src/main/java/com/example/presentation/weather/weatherScreen/WeatherUIModel.kt
+++ presentation/src/main/java/com/example/presentation/weather/weatherScreen/WeatherUIModel.kt
@@ -1,23 +1,23 @@
 package com.example.presentation.weather.weatherScreen
 
 data class WeatherUIModel(
-    val main: Main,
+    val main: Main?,
 
-    val name: String,
+    val name: String?,
 
-    val weather: List<Weather>,
+    val weather: List<Weather?>?,
 ) {
     data class Main(
-        val humidity: Int, // 습도
-        val pressure: Int, // 해수면 기압
-        val temp: Double, // 온도
-        val tempMax: Double, // 최대 기온
-        val tempMin: Double, // 최저 기온
+        val humidity: Int?, // 습도
+        val pressure: Int?, // 해수면 기압
+        val temp: Double?, // 온도
+        val tempMax: Double?, // 최대 기온
+        val tempMin: Double?, // 최저 기온
     )
 
     data class Weather(
-        val description: String,
-        val icon: String,
-        val main: String
+        val description: String?,
+        val icon: String?,
+        val main: String?
     )
 }
